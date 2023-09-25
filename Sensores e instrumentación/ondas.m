b66d3a845040358bf6a7d243c9e07b2fbf8c4ef2
Ignacio Andrade Salazar <ignacio.andrade8033@alumnos.udg.mx>
2023-09-25 11:09:24 -0600

    ya esta wey

---
+++ Sensores e instrumentación/ondas.m
@@ -0,0 +1,30 @@
+clc;
+clear all;
+Fs=200e3;
+Ts=1/Fs;
+dt = 0:Ts:5e-3-Ts;
+f1=1e3;
+f2=20e3;
+f3=5e3;
+f4=15e3;
+f5=25e3;
+f6=30e3;
+f7=35e3;
+f8=40e3;
+f9=45e3;
+f10=50e3;
+y=5*sin(2*pi*f1*dt)+10*sin(2*pi*f2*dt)+15*sin(2*pi*f3*dt)+20*sin(2*pi*f4*dt)+25*sin(2*pi*f5*dt)+30*sin(2*pi*f6*dt)+35*sin(2*pi*f7*dt)+40*sin(2*pi*f8*dt)+45*sin(2*pi*f9*dt)+50*sin(2*pi*f10*dt)
+figure(1)
+plot(dt,y);
+
+nfft=length(y);
+nfft2=2.^nextpow2(nfft);
+
+fy=fft(y,nfft2);
+
+fy=fy(1:nfft2/2);
+
+xfft=Fs.*(0:nfft2/2-1)/nfft2;
+
+figure(2)
+plot(xfft,abs(fy/max(fy)));
