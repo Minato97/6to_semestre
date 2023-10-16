cdc249dcf166769cc07e01e112ab5929a5022528
Ignacio Andrade Salazar <ignacio.andrade8033@alumnos.udg.mx>
2023-10-16 15:01:38 -0600

    antologia

--- Teoria del control/modelado.m
+++ Teoria del control/modelado.m
@@ -2,10 +2,12 @@
 %Gs=tf(1,[R*C 1]);
 %step(Gs);
 
-R= 1e3;
-C=47e-6;
-L=10e-3;
+R1= 1e3;
+C1=10e-6;
+R2= 10e3;
+C2= 4.7e-6
+%L=10e-3;
 
-Gs=tf(1,[L*C R*C 1]);
+Gs=tf(1,[R1*C1*R2*C2 R1*C1+R1*C2+R2*C2 1]);
 
 step(Gs);
